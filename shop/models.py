716d11113d036de374f4aa0fcda8e6dbad5fb3bf
Haris Ali Khan <harisalikhan13@gmail.com>
2019-02-10 01:25:25 +0530

    Django Video No 27

--- shop/models.py
+++ shop/models.py
@@ -4,5 +4,12 @@ from django.db import models
 class Product(models.Model):
     product_id = models.AutoField
     product_name = models.CharField(max_length=50)
+    category = models.CharField(max_length=50, default="")
+    subcategory = models.CharField(max_length=50, default="")
+    price = models.IntegerField(default=0)
     desc = models.CharField(max_length=300)
     pub_date = models.DateField()
+    image = models.ImageField(upload_to='shop/images', default="")
+
+    def __str__(self):
+        return self.product_name
