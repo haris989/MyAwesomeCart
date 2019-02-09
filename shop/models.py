8b1502310006a3a72cf710b6097ec46601bedd44
Haris Ali Khan <harisalikhan13@gmail.com>
2019-02-10 00:06:09 +0530

    Initial commit

---
+++ shop/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+class Product(models.Model):
+    product_id = models.AutoField
+    product_name = models.CharField(max_length=50)
+    desc = models.CharField(max_length=300)
+    pub_date = models.DateField()
---
+++ shop/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+
+def index(request):
+    return render(request, 'shop/index.html')
+
+def about(request):
+    return HttpResponse("We are at about")
+
+def contact(request):
+    return HttpResponse("We are at contact")
+
+def tracker(request):
+    return HttpResponse("We are at tracker")
+
+def search(request):
+    return HttpResponse("We are at search")
+
+def productView(request):
+    return HttpResponse("We are at product view")
+
+def checkout(request):
+    return HttpResponse("We are at checkout")
