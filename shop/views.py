17aa1a3a94bd8d4ef678c2e97379afefb5df4b42
Haris Ali Khan <harisalikhan13@gmail.com>
2019-02-12 01:36:19 +0530

    django 32

--- shop/views.py
+++ shop/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
+from .models import Product
+from math import ceil
 
 # Create your views here.
 from django.http import HttpResponse
 
 def index(request):
-    return render(request, 'shop/index.html')
+    products = Product.objects.all()
+    print(products)
+    n = len(products)
+    nSlides = n//4 + ceil((n/4)-(n//4))
+    params = {'no_of_slides':nSlides, 'range': range(1,nSlides),'product': products}
+    return render(request, 'shop/index.html', params)
 
 def about(request):
     return render(request, 'shop/about.html')
