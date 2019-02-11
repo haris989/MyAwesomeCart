d302eca7f7d03df685bfa8408956018c0b74c00a
Haris Ali Khan <harisalikhan13@gmail.com>
2019-02-12 00:00:58 +0530

    Tutorial 31

--- shop/views.py
+++ shop/views.py
@@ -7,7 +7,7 @@ def index(request):
     return render(request, 'shop/index.html')
 
 def about(request):
-    return HttpResponse("We are at about")
+    return render(request, 'shop/about.html')
 
 def contact(request):
     return HttpResponse("We are at contact")
