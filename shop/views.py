245260b504fa900b8a17267bb6363419d06c5209
Haris Ali Khan <harisalikhan13@gmail.com>
2019-02-16 00:07:53 +0530

    django 40

--- shop/views.py
+++ shop/views.py
@@ -30,16 +30,16 @@ def about(request):
     return render(request, 'shop/about.html')
 
 def contact(request):
-    return HttpResponse("We are at contact")
+    return render(request, 'shop/contact.html')
 
 def tracker(request):
-    return HttpResponse("We are at tracker")
+    return render(request, 'shop/tracker.html')
 
 def search(request):
-    return HttpResponse("We are at search")
+    return render(request, 'shop/search.html')
 
 def productView(request):
-    return HttpResponse("We are at product view")
+    return render(request, 'shop/prodView.html')
 
 def checkout(request):
-    return HttpResponse("We are at checkout")
+    return render(request, 'shop/checkout.html')
