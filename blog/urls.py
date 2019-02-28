ee65070553e853e0d88f708b56e548edc79f0f24
Haris Ali Khan <harisalikhan13@gmail.com>
2019-03-01 00:09:23 +0530

    django 61

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="blogHome"),
-    path("blogpost/", views.blogpost, name="blogHome")
+    path("blogpost/<int:id>", views.blogpost, name="blogHome")
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
-
+from .models import Blogpost
 # Create your views here.
 from django.http import HttpResponse
 
 def index(request):
     return render(request, 'blog/index.html')
 
-def blogpost(request):
-    return render(request, 'blog/blogpost.html')
+def blogpost(request, id):
+    post = Blogpost.objects.filter(post_id = id)[0]
+    print(post)
+    return render(request, 'blog/blogpost.html',
+                  {'post':post})
