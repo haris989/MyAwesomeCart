41b16b5eb298547ffac0210fc64a33cae9140890
Haris Ali Khan <harisalikhan13@gmail.com>
2019-02-28 23:33:56 +0530

    Django 59

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="ShopHome")
+    path("", views.index, name="blogHome"),
+    path("blogpost/", views.blogpost, name="blogHome")
 ]
--- blog/views.py
+++ blog/views.py
@@ -6,3 +6,5 @@ from django.http import HttpResponse
 def index(request):
     return render(request, 'blog/index.html')
 
+def blogpost(request):
+    return render(request, 'blog/blogpost.html')
