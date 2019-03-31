c27744c8434067a747cf80e9fa535d719a03daf6
Harry <harry@codewithharry.com>
2019-03-31 12:52:46 +0530

    Django 67

--- shop/views.py
+++ shop/views.py
@@ -20,6 +20,31 @@ def index(request):
     params = {'allProds':allProds}
     return render(request, 'shop/index.html', params)
 
+def searchMatch(query, item):
+    '''return true only if query matches the item'''
+    if query in item.desc.lower() or query in item.product_name.lower() or query in item.category.lower():
+        return True
+    else:
+        return False
+
+def search(request):
+    query = request.GET.get('search')
+    allProds = []
+    catprods = Product.objects.values('category', 'id')
+    cats = {item['category'] for item in catprods}
+    for cat in cats:
+        prodtemp = Product.objects.filter(category=cat)
+        prod = [item for item in prodtemp if searchMatch(query, item)]
+
+        n = len(prod)
+        nSlides = n // 4 + ceil((n / 4) - (n // 4))
+        if len(prod) != 0:
+            allProds.append([prod, range(1, nSlides), nSlides])
+    params = {'allProds': allProds, "msg": ""}
+    if len(allProds) == 0 or len(query)<4:
+        params = {'msg': "Please make sure to enter relevant search query"}
+    return render(request, 'shop/search.html', params)
+
 
 def about(request):
     return render(request, 'shop/about.html')
@@ -57,10 +82,6 @@ def tracker(request):
             return HttpResponse('{}')
 
     return render(request, 'shop/tracker.html')
-
-
-def search(request):
-    return render(request, 'shop/search.html')
 
 
 def productView(request, myid):
